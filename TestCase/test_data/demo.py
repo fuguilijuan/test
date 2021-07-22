ca49aac91fbddf64c5d7499dcde7cedfe23e9417
guifu <48000926+fugui66@users.noreply.github.com>
2021-07-22 15:29:12 +0800

    Delete TestCase directory

--- TestCase/test_data/demo.py
+++
@@ -1,9 +0,0 @@
-import requests
-import urllib.request
-url = "http://192.168.6.201:8081"
-
-try:
-    r = urllib.request.urlopen(url,timeout=3).read()
-    print(r)
-except Exception as e:
-    print('False:'+str(e))
